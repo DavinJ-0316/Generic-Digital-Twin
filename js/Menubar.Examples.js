f919f18fd7a63d00562271ff7bcc298e23e56a91
Jinhao Jiang <58126838+DavinJ-0316@users.noreply.github.com>
2021-09-22 13:58:12 +1000

    Update Menubar.Examples.js

--- js/Menubar.Examples.js
+++ js/Menubar.Examples.js
@@ -7,23 +7,13 @@ Menubar.Examples = function(editor) {
                    var title = new UI.Panel();
                    title.setClass('title');
                    title.setTextContent('Article');
-                   container.add(title);
-      
-                   var options = new UI.Panel();
-                   options.setClass('options');
-                   container.add(options);
-      
-      
-                   var option = new UI.Row();
-                   option.setClass('option');
-                   option.setTextContent('View');
-      
-                   option.onClick(function() {
-                    
+                   title.onClick(function() {
+
                         window.open("https://link.springer.com/chapter/10.1007/978-3-030-82529-4_21", "_blank")
-                           
+
                    });
-                   options.add(option);
+                   container.add(title);
+     
          
          return container;
    
