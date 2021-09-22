878c065959ed3d2d1873207646fb565c9649ec0b
Jinhao Jiang <58126838+DavinJ-0316@users.noreply.github.com>
2021-09-22 13:32:04 +1000

    Update Menubar.Examples.js

--- js/Menubar.Examples.js
+++ js/Menubar.Examples.js
@@ -1,16 +1,29 @@
 Menubar.Examples = function(editor) {
 
-
-	var option = new UIRow();
-	option.setClass( 'option' );
-	option.setTextContent('View Journal');
-	option.onClick( function () {
-
-		window.open( 'https://link.springer.com/chapter/10.1007/978-3-030-82529-4_21', '_blank' );
-
-	} );
-	options.add( option );
-
-	return container;
+         
+                   var container = new UI.Panel();
+                   container.setClass('menu');
+      
+                   var title = new UI.Panel();
+                   title.setClass('title');
+                   title.setTextContent('View Journal');
+                   container.add(title);
+      
+                   var options = new UI.Panel();
+                   options.setClass('options');
+                   container.add(options);
+      
+      
+                   var option = new UI.Row();
+                   option.setClass('option');
+                   option.setTextContent('View Journal');
+      
+                   option.onClick(function() {
+                    
+                        window.open("https://link.springer.com/chapter/10.1007/978-3-030-82529-4_21", "_blank")
+                           
+                   });
+                   options.add(option);
+   
 
 };
