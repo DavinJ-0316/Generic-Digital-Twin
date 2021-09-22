b338b84ad87c90cdb9c7c1ec6cddc97937e3febd
Jinhao Jiang <58126838+DavinJ-0316@users.noreply.github.com>
2021-09-22 13:53:05 +1000

    Update Menubar.Examples.js

--- js/Menubar.Examples.js
+++ js/Menubar.Examples.js
@@ -6,7 +6,7 @@ Menubar.Examples = function(editor) {
       
                    var title = new UI.Panel();
                    title.setClass('title');
-                   title.setTextContent('View Journal Article');
+                   title.setTextContent('Journal Article');
                    container.add(title);
       
                    var options = new UI.Panel();
@@ -16,7 +16,7 @@ Menubar.Examples = function(editor) {
       
                    var option = new UI.Row();
                    option.setClass('option');
-                   option.setTextContent('Journal Article');
+                   option.setTextContent('View paper');
       
                    option.onClick(function() {
                     
